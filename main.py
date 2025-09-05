8b8a9df5c61e84326c37b1d6982760e332f7e294
Sheikh Ajim Bin Nazir <sheikhajimbinnazir@gmail.com>
2025-09-05 19:52:04 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,51 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.lang import Builder
 
-class MainApp(App):
-    def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
-        main_layout = BoxLayout(orientation="vertical")
-        self.solution = TextInput(
-            multiline=False, readonly=True, halign="right", font_size=55
-        )
-        main_layout.add_widget(self.solution)
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
-        ]
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text=label,
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.utils.set_bars_colors import set_bars_colors
 
-        equals_button = Button(
-            text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
-        )
-        equals_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equals_button)
 
-        return main_layout
+class SampleApp(MDApp):
 
-    def on_button_press(self, instance):
-        current = self.solution.text
-        button_text = instance.text
+    def __init__(self, **kwargs) -> None:
+        super(SampleApp, self).__init__(**kwargs)
+        self.theme_cls.primary_palette = "Darkblue"
 
-        if button_text == "C":
-            # Clear the solution widget
-            self.solution.text = ""
+    def build(self) -> MDScreen:
+        self.appKv="""
+MDScreen:
+    MDButton:
+        style: 'tonal'
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        on_press:
+            app.apply_styles("Light") if (not app.theme_cls.theme_style == "Light") else app.apply_styles("Dark")
+
+        MDButtonText:
+            text: 'Hello, World!'
+"""
+        AppScreen = Builder.load_string(self.appKv)
+        self.apply_styles("Light")
+        return AppScreen
+
+    def apply_styles(self, style: str = "Light") -> None:
+        self.theme_cls.theme_style = style
+        if style == "Light":
+            Window.clearcolor = status_color = nav_color = app.theme_cls.surfaceColor
+            style = "Dark"
         else:
-            if current and (
-                self.last_was_operator and button_text in self.operators):
-                # Don't add two operators right after each other
-                return
-            elif current == "" and button_text in self.operators:
-                # First character cannot be an operator
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text = new_text
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
+            Window.clearcolor = status_color = nav_color = app.theme_cls.surfaceColor
+            style = "Light"
+        self.set_bars_colors(status_color, nav_color, style)
 
-    def on_solution(self, instance):
-        text = self.solution.text
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
-
+    def set_bars_colors(self, status_color: list[float] = [1.0, 1.0, 1.0, 1.0], nav_color: list[float] = [1.0, 1.0, 1.0, 1.0], style: str = "Dark") -> None:
+        set_bars_colors(
+            status_color,  # status bar color
+            nav_color,  # navigation bar color
+            style,  # icons style of status and navigation bar
+        )
 
 if __name__ == "__main__":
-    app = MainApp()
+    app = SampleApp()
     app.run()
+    
